7bf53ed96e2e5a2128b815bc8a6605fa16dfccd6
Jarrad Miller <jm020801@gmail.com>
2019-01-31 16:57:05 -0700

    MVP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 server.listen(9000, () => {
-    console.log("\n** server up on port 9000 **\n");
-}) 
+  console.log("\n** server up on port 9000 **\n");
+});
